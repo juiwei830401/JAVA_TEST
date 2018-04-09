432062a129c4526057268a247bdbcc82dd2f2f05
juiwei830401 <31119875+juiwei830401@users.noreply.github.com>
2018-04-09 11:55:53 +0800

    Update TEST.java

--- .gitignore/TEST.java
+++ .gitignore/TEST.java
@@ -4,7 +4,7 @@ import javax.mail.internet.*;
  
 public class SendEmail {
      public static void main(String args[]){
-         String msgBody = "這是 JavaMail APIs.傳送的訊息\n陳威文 上";
+         String msgBody = "這是 JavaMail APIs.傳送的訊息\n李俊緯 上";
          Properties props = new Properties();
          props.put("mail.smtp.host", "mail.ncku.edu.tw"); //設定SMTP Server
          Session session = Session.getDefaultInstance(props, null); //取得與SMTP Server的連線
@@ -12,17 +12,17 @@ public class SendEmail {
               Message msg = new MimeMessage(session); //取得一Mime的Message
             //設定自己的email帳號
               InternetAddress from = new InternetAddress("cww5@mail.ncku.edu.tw");
-              // InternetAddress from = new InternetAddress("cww5@mail.ncku.edu.tw", “陳威文”);
+              // InternetAddress from = new InternetAddress("juiwei830401@gmail.com", "李俊緯");
               msg.setFrom(from);
              //設定接收者的帳號，可以有多個帳號同時傳送
-              InternetAddress[] address = {new InternetAddress("cww@mail.hosp.ncku.edu.tw")};
+              InternetAddress[] address = {new InternetAddress("juiwei830401@gmail.com")};
               msg.setRecipients(Message.RecipientType.TO, address);
              /* 有需要時，可以加入CC /BCC，
                 如果只一位receipt可用
                 Address addr = new InternetAddress("cww@mail.hosp.ncku.edu.tw")
                 msg.serReceipt(Message.RecipientType.TO, addr)
-                InternetAddress ccAddress = new InternetAddress("tlchen@mail.ncku.edu.tw");
-                InternetAddress bccAddress = new InternetAddress("jjh@mail.ncku.edu.tw");
+                InternetAddress ccAddress = new InternetAddress("juiwei830401@gmail.com");
+                InternetAddress bccAddress = new InternetAddress("juiwei830401@gmail.com");
                 msg.addRecipient(Message.RecipientType.CC, ccAddress);
                 msg.addRecipient(Message.RecipientType.BCC, bccAddress);
              */
@@ -30,12 +30,12 @@ public class SendEmail {
               msg.setContent(msgBody, "text/plain;charset=big5");
               Transport.send(msg);
              /*也可以如此做
-msg.saveChanges(); 
-Transport transport = session.getTransport("smtp");
-transport.connect(“mail.ncku.edu.tw”, “UID”, “PWD”);
-transport.sendMessage(msg, msg.getAllRecipients());
-transport.close();
-            */
+               msg.saveChanges(); 
+               Transport transport = session.getTransport("smtp");
+               transport.connect(“mail.ncku.edu.tw”, “UID”, “PWD”);
+               transport.sendMessage(msg, msg.getAllRecipients());
+               transport.close();
+              */
          } catch (MessagingException mex) {mex.printStackTrace();
 } catch (Exception e) {}
      }
